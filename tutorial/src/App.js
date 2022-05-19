46af7175c1fca931602a2e06ab1c87a1d34b840b
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-05-19 17:13:42 +0530

    day -2

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+
 import './App.css';
+import About from './components/About';
+import Navbar from './components/Navbar';
+// import TextForm from './components/TextForm';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar title="prop titles"  aboutText="prop about" />
+    <div className="container">
+    {/* <TextForm heading="need a heading" /> */}
+    <About></About>
     </div>
+    </>
   );
 }
 
 export default App;
+
---
+++ tutorial/src/components/TextForm.js
@@ -0,0 +1,58 @@
+import React,{useState} from 'react'
+
+export default function TextForm(props) {
+
+    const handleupclick = () =>{
+        // console.log("uppercase was clicked" + text)
+        let newText = text.toUpperCase();
+        setText(newText)
+        // setToggle(!toggle)
+    }
+
+    const handleloclick = () =>{
+        // console.log("uppercase was clicked" + text)
+        let newText = text.toLowerCase();
+        setText(newText)
+        // setToggle(!toggle)
+    }
+
+    const handleclearclick = () =>{
+        let newtext = " "
+        setText(newtext)
+    }
+
+    const handleonchange = (event) =>{
+        // console.log("on change")
+        setText(event.target.value)
+        
+    }
+
+    const [text, setText] = useState("");
+    // const [toggle, setToggle] = useState(true)
+  return (
+    <>
+        <div className='container'>
+        
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" id="myBox" placeholder='Enter your text' value={text} onChange={handleonchange} rows="8"></textarea>
+        </div>
+        
+        <button className="btn btn-info mx-1" onClick={handleupclick} >Convert to uppercase</button>
+        <button className="btn btn-info mx-1" onClick={handleloclick} >Convert to lowercase</button>
+        
+        
+        
+        <button className="btn btn-info mx-1" onClick={handleclearclick} >clear text</button>
+        
+        </div>
+        <div className="container">
+            <h2>Your text summary</h2>
+            <p>{text.split(" ").length} words and {text.length} characters </p>
+            <p>{0.008 * text.split(" ").length } minutes read</p>
+            <h2>preview</h2>
+            <p>{text}</p>
+        </div>
+    </>
+  )
+}
