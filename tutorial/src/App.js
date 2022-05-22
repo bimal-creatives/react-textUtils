8d5131c259ca613cbcc99f3ca6842cf5b1cd2187
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-05-22 21:46:15 +0530

    neel bhaiya solved it

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,18 +1,13 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-// import About from './components/About';
-import React, { useState } from 'react';
-import Alert from './components/Alert';
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route
-// } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import About from "./components/About";
+import React, { useState } from "react";
+// import Alert from "./components/Alert";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
- 
 function App() {
-  const [mode, setMode] = useState('light'); // Whether dark mode is enabled or not
+  const [mode, setMode] = useState("light"); // Whether dark mode is enabled or not
   // const [alert, setAlert] = useState(null);
 
   // const showAlert = (message, type)=>{
@@ -25,51 +20,55 @@ function App() {
   //     }, 1500);
   // }
 
-  const removeBodyClasses = ()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-success') 
-  }
+  const removeBodyClasses = () => {
+    document.body.classList.remove("bg-light");
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-danger");
+    document.body.classList.remove("bg-success");
+  };
 
-  const toggleMode = (cls)=>{
+  const toggleMode = (cls) => {
     removeBodyClasses();
-    console.log(cls)
-    document.body.classList.add('bg-'+cls)
-    if(mode === 'light'){
-      setMode('dark');
-      document.body.style.backgroundColor = '#042743';
-      // showAlert("Dark mode has been enabled", "success"); 
-    }
-    else{
-      setMode('light');
-      document.body.style.backgroundColor = 'white';
+    console.log(cls);
+    document.body.classList.add("bg-" + cls);
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      // showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
       // showAlert("Light mode has been enabled", "success");
       // document.title = 'TextUtils - Light Mode';
     }
-  }
+  };
   return (
     <>
-    {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
-    {/* <Navbar/> */}
-    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    <Alert alert={alert}/>
-    <div className="container my-3">
-    {/* <Router>
-    <Routes>
-    /users --> Component 1
-        /users/home --> Component 2
-          <Route exact path="/about">
-            <About mode={mode} />
-          </Route>
-          <Route exact path="/">
-          </Route>
-    </Routes>
-    </Router> */}
-            <TextForm heading="Try TextUtils - word counter, character counter, remove extra spaces" mode={mode}/>
-    </div>
-    </> 
+      {/* <Navbar title="TextUtils" aboutText="About TextUtils" /> */}
+      {/* <Navbar/> */}
+      <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        {/* <Alert alert={alert}/> */}
+        <div className="container my-3">
+          <Routes>
+            {/* /users --> Component 1
+        /users/home --> Component 2 */}
+            <Route exact path="/about" element={<About mode={mode} />}></Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  heading="Try TextUtils - word counter, character counter, remove extra spaces"
+                  mode={mode}
+                />
+              }
+            ></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
