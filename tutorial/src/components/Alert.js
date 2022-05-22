0c76f9cd5964e960270e207c6f10b755c833ed0c
bimal-creatives <72511459+bimal-creatives@users.noreply.github.com>
2022-05-22 19:26:35 +0530

    day-5

--- tutorial/src/components/Alert.js
+++ tutorial/src/components/Alert.js
@@ -3,9 +3,9 @@ import React from 'react'
 function Alert(props) {
   return (
     <div>
-        props.alert && <div className="alert alert-warning alert-dismissible fade show" role="alert">
-        <strong>{props.alert.type}</strong> : {props.alert.msg}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        <div className="alert alert-warning alert-dismissible fade show" role="alert">
+        <strong>{props.alert.msg}</strong> : {props.alert.msg}
+        <button type="button" className="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>
   )
--- tutorial/src/components/TextForm.js
+++ tutorial/src/components/TextForm.js
@@ -1,75 +1,71 @@
-import React,{useState} from 'react'
+import React, {useState} from 'react'
+
 
 export default function TextForm(props) {
-
-    const handleupclick = () =>{
-        // console.log("uppercase was clicked" + text)
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked: " +  text);
         let newText = text.toUpperCase();
         setText(newText)
-        
-        
-        // setToggle(!toggle)
+        props.showAlert("Converted to uppercase!", "success");
     }
-    
-    const handleloclick = () =>{
-        // console.log("uppercase was clicked" + text)
+
+    const handleLoClick = ()=>{ 
         let newText = text.toLowerCase();
         setText(newText)
-        
-        // setToggle(!toggle)
+        props.showAlert("Converted to lowercase!", "success");
     }
-    
-    const handleclearclick = () =>{
-        let newtext = " "
-        setText(newtext)
+
+    const handleClearClick = ()=>{ 
+        let newText = '';
+        setText(newText);
+        props.showAlert("Text Cleared!", "success");
     }
-    
-    const handleonchange = (event) =>{
-        // console.log("on change")
-        setText(event.target.value)
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value) 
     }
-    
+
+    // Credits: A
     const handleCopy = () => {
+        console.log("I am copy");
         var text = document.getElementById("myBox");
-        text.select();
+        text.select(); 
         navigator.clipboard.writeText(text.value);
-        
+        document.getSelection().removeAllRanges();
+        props.showAlert("Copied to Clipboard!", "success");
     }
-    
+
+    // Credits: Coding Wala
     const handleExtraSpaces = () => {
         let newText = text.split(/[ ]+/);
-        setText(newText.join(" "))
-       
+        setText(newText.join(" "));
+        props.showAlert("Extra spaces removed!", "success");
     }
-    
-    const [text, setText] = useState("");
-    // const [toggle, setToggle] = useState(true)
-  return (
-    <>
-        <div className='container' style={{ color:props.mode=== 'dark'?'white': '#112B3C'}}>
-        
-        <h1>{props.heading}</h1>
-        <div className="mb-3">
-        <textarea className="form-control" id="myBox" style={{ color:props.mode=== 'dark'?'white': '#112B3C', backgroundColor:props.mode=== 'dark'?'#21325E':'#C4DDFF', borderColor:props.mode==='dark'?'#21325E':'#C4DDFF'}} placeholder='Enter your text' value={text} onChange={handleonchange}  rows="8"></textarea>
+
+    const [text, setText] = useState(''); 
+    // text = "new text"; // Wrong way to change the state
+    // setText("new text"); // Correct way to change the state
+    return (
+        <>
+        <div className="container" style={{color: props.mode==='dark'?'white':'#042743'}}> 
+            <h1 className='mb-4'>{props.heading}</h1>
+            <div className="mb-3"> 
+            <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'#13466e':'white', color: props.mode==='dark'?'white':'#042743'}} id="myBox" rows="8"></textarea>
+            </div>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+            <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
         </div>
-        
-        <button className="btn btn-primary mx-1" onClick={handleupclick} >Convert to uppercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleloclick} >Convert to lowercase</button>
-        <button className="btn btn-primary mx-1" onClick={handleclearclick} >clear text</button>
-        <button className="btn btn-primary mx-1" onClick={handleCopy} >copy text</button>
-        <button className="btn btn-primary mx-1" onClick={handleExtraSpaces} >Remove extra space</button>
-        
-        
-        
-        
-        </div>
-        <div className="container" style={{ color:props.mode=== 'dark'?'white': '#112B3C'}}>
+        <div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
             <h2>Your text summary</h2>
-            <p>{text.split(" ").length} words and {text.length} characters </p>
-            <p>{0.008 * text.split(" ").length } minutes read</p>
-            <h2>preview</h2>
-            <p>{text.length>0?text:"enter something to preview"}</p>
+            <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
+            <p>{0.008 *  text.split(" ").filter((element)=>{return element.length!==0}).length} Minutes read</p>
+            <h2>Preview</h2>
+            <p>{text.length>0?text:"Nothing to preview!"}</p>
         </div>
-    </>
-  )
+        </>
+    )
 }
